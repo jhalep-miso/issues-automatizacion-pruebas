e988c85dd2c5ed94c70c66ef91e918da4c2d02fd
Alejandro Peña <ja.penat1@uniandes.edu.co>
2024-04-30 08:30:33 -0500

    Initial commit

---
+++ features/web/support/hooks.js
@@ -0,0 +1,16 @@
+const { After, Before, BeforeAll } = require("@cucumber/cucumber");
+const { WebClient } = require("kraken-node");
+const dns = require("dns");
+
+BeforeAll(async function () {
+  dns.setDefaultResultOrder("ipv4first");
+});
+
+Before(async function () {
+  this.deviceClient = new WebClient("chrome", {}, this.userId);
+  this.driver = await this.deviceClient.startKrakenForUserId(this.userId);
+});
+
+After(async function () {
+  await this.deviceClient.stopKrakenForUserId(this.userId);
+});
