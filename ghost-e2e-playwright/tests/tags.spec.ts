1d83090c053cc47023a30c9443f070979cd33758
Sebastian Lemus Cadena <sebastian.slc9@gmail.com>
2024-05-05 10:46:19 -0500

    Completes scenario 11.

--- ghost-e2e-playwright/tests/tags.spec.ts
+++ ghost-e2e-playwright/tests/tags.spec.ts
@@ -32,3 +32,37 @@ test("Create multiple Post with the same Tag", async ({ page }) => {
 
   expect(postTitles.sort()).toStrictEqual(posts.map(p => p.title).sort())
 })
+
+test("Create a tag and associate it with multiple posts", async ({ page }) => {
+  const tag = {
+    name: faker.word.words(1),
+    slug: faker.word.words(1),
+    description: faker.word.words(50)
+  }
+  const randomPost = () => ({
+    title: faker.word.words(5),
+    content: faker.word.words(50),
+    tags: [tag.name]
+  })
+  const posts = Array.from({ length: 4 }, randomPost)
+
+  const adminPage = new AdminPage(page)
+  const tagsPage = await adminPage.tags()
+  const newTagPage = await tagsPage.newTag()
+
+  await newTagPage.create(tag)
+  await adminPage.go()
+
+  const postsPage = await adminPage.posts()
+  for (const post of posts) {
+    const newPostPage = await postsPage.newPost()
+    await newPostPage.create(post)
+    await postsPage.go()
+  }
+
+  await tagsPage.go()
+  await tagsPage.goToPostsByTagName(tag.name)
+  const postTitles = await postsPage.getAllPostTitles()
+
+  expect(postTitles.sort()).toStrictEqual(posts.map(p => p.title).sort())
+})
