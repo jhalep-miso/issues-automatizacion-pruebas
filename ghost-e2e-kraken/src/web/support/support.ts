c58670cf651ffff5b8638f25f477ac57c05f1e83
romeluniandes <r.palominoj@uniandes.edu.co>
2024-05-04 19:21:57 -0500

    Feature | Create scenario for pages section | Romel Palomino

--- ghost-e2e-kraken/src/web/support/support.ts
+++ ghost-e2e-kraken/src/web/support/support.ts
@@ -1,34 +1,36 @@
-import {
-  setWorldConstructor,
-  setDefaultTimeout,
-  World,
-  IWorldOptions,
-} from "@cucumber/cucumber";
-import { LoginPage } from "../page-objects/login-page";
-import { PostPage } from "../page-objects/post-page";
+import {IWorldOptions, setDefaultTimeout, setWorldConstructor, World,} from "@cucumber/cucumber";
+import {LoginPage} from "../page-objects/login-page";
+import {PostPage} from "../page-objects/post-page";
+import {PageSection} from "../page-objects/page-section";
+import {ErrorPage} from "../page-objects/error-page";
 
 export class KrakenWorld extends World {
-  userId: any;
-  device: any;
-  testScenarioId: any;
-  attach: any;
-  driver: any;
-  deviceClient: any;
-  loginPage!: LoginPage;
-  postPage!: PostPage;
-  constructor(input: IWorldOptions) {
-    super(input);
-    let params = input.parameters;
-    this.userId = params.id;
-    this.device = params.device || {};
-    this.testScenarioId = params.testScenarioId;
-    this.attach = input.attach;
-  }
+    userId: any;
+    device: any;
+    testScenarioId: any;
+    attach: any;
+    driver: any;
+    deviceClient: any;
+    loginPage!: LoginPage;
+    postPage!: PostPage;
+    pageSection!: PageSection;
+    errorPage!: ErrorPage;
 
-  async init() {
-    this.loginPage = new LoginPage(this.driver);
-    this.postPage = new PostPage(this.driver);
-  }
+    constructor(input: IWorldOptions) {
+        super(input);
+        let params = input.parameters;
+        this.userId = params.id;
+        this.device = params.device || {};
+        this.testScenarioId = params.testScenarioId;
+        this.attach = input.attach;
+    }
+
+    async init() {
+        this.loginPage = new LoginPage(this.driver);
+        this.postPage = new PostPage(this.driver);
+        this.pageSection = new PageSection(this.driver);
+        this.errorPage = new ErrorPage(this.driver);
+    }
 }
 
 setWorldConstructor(KrakenWorld);
