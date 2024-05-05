d5cb4f1e97184cb7afd66b41c1e58b9bbe1ad300
Sebastian Lemus Cadena <sebastian.slc9@gmail.com>
2024-05-05 14:35:43 -0500

    Completes all test scenarios

--- ghost-e2e-playwright/tests/pages/members/ImpersonatePage.ts
+++ ghost-e2e-playwright/tests/pages/members/ImpersonatePage.ts
@@ -5,7 +5,10 @@ export class ImpersonatePage {
   constructor(private readonly page: Page) { }
 
   async editName(name: string): Promise<void> {
+    // Sometimes the following fails because a pop up gets in the way of the click
     await this.page.getByRole("link", { name: "Account" }).click()
+
+    await this.page.waitForTimeout(2000)
 
     const frame = this.page.frameLocator('[data-testid="portal-popup-frame"]')
 
