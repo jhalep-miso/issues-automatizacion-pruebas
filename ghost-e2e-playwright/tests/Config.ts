4c51d6018c02e0df0923b738bab7eef08a267fd6
Sebastian Lemus Cadena <sebastian.slc9@gmail.com>
2024-05-05 17:03:14 -0500

    Wait for response when creating a Post

--- ghost-e2e-playwright/tests/Config.ts
+++ ghost-e2e-playwright/tests/Config.ts
@@ -1,15 +1,15 @@
-// export const Config = {
-//   baseUri: "https://ghost-ebcl.onrender.com",
-//   user: {
-//     email: "r.palominoj@uniandes.edu.co",
-//     password: "569RiutNDapHbzv"
-//   }
-// }
-
 export const Config = {
-  baseUri: "http://localhost:8080",
+  baseUri: "https://ghost-ebcl.onrender.com",
   user: {
-    email: "s.lemus@uniandes.edu.co",
-    password: "abc123def456#"
+    email: "r.palominoj@uniandes.edu.co",
+    password: "569RiutNDapHbzv"
   }
 }
+
+// export const Config = {
+//   baseUri: "http://localhost:8080",
+//   user: {
+//     email: "s.lemus@uniandes.edu.co",
+//     password: "abc123def456#"
+//   }
+// }
