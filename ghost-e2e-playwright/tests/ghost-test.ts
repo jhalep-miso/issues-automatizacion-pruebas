4a508f4294b43cae3e0fd6327e01919f892243dd
Sebastian Lemus Cadena <sebastian.slc9@gmail.com>
2024-05-18 20:53:12 -0500

    Adds documentation on the integration of the tests with the data generation strategies

--- ghost-e2e-playwright/tests/ghost-test.ts
+++ ghost-e2e-playwright/tests/ghost-test.ts
@@ -5,6 +5,10 @@ export type DataGenerationMode = "apriori" | "pseudo" | "random"
 
 export const genModes: DataGenerationMode[] = ["apriori", "pseudo", "random"]
 
+/**
+ * An extension of playwright `test` function to expose a DataGenerationProvider for each
+ * test defined with this function
+ */
 export const test = base.extend<{ dataProvider: DataGenerationProvider }>({
   dataProvider: async ({ page }, use) => {
     const provider = await TestData.getProvider()
@@ -12,6 +16,12 @@ export const test = base.extend<{ dataProvider: DataGenerationProvider }>({
   }
 })
 
+/**
+ * A lazy variable that allocates and memoizes a DataGenerationProvider. The first time
+ * `getProvider` gets called, it initializes a DataGenerationProvider and stores it in the
+ * `provider` variable. Subsequence calls of the method will return the same instance without
+ * having to initialize it again
+ */
 class TestDataRef {
   private provider: DataGenerationProvider
 
@@ -22,6 +32,11 @@ class TestDataRef {
   }
 }
 
+/**
+ * A class that provides one of the available data generation strategies: "apriori" | "pseudo" | "random"
+ * On creation, it will initialize all strategies, and will expose a `select` map from DataGenerationMode to
+ * a particular DataGenerator
+ */
 export class DataGenerationProvider {
 
   private constructor(readonly select: Record<DataGenerationMode, DataGenerator>) { }
